a61ee95f7143b38206e58485f759a66be067c106
Zhuan Akbar <zhuan.akb@gmail.com>
2020-10-08 12:27:11 +0700

    adding validator for register

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const { body } = require('express-validator');
 const authController = require('../controller/auth-controller');
 
 const router = express.Router();
 
 router.post('/', authController.login);
-router.put('/signup', authController.singup);
+router.put(
+  '/signup',
+  [
+    body('username')
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage({ message: 'Username harus 3 karakter atau lebih' })
+      .notEmpty(),
+    body('password')
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage({ message: 'Password harus 5 karakter atau lebih' })
+      .notEmpty(),
+    body('email')
+      .isEmail()
+      .withMessage({ message: 'Masukan email sesuai format' })
+      .normalizeEmail(),
+  ],
+  authController.singup,
+);
 
 module.exports = router;
